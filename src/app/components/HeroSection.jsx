a15fda6fbdef90014c6fc837402b2261c8952bb4
Your Name <jonathanliang88@gmail.com>
2024-03-25 00:22:56 -0400

    linked resume/cv and adjusted padding for intro

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
                         <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
                             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello, I&apos;m {" "}</span>
                             <br></br>
-                            <div style={{ minHeight: '2em' }}> {/* Adjust '3em' as needed */}
+                            <div style={{ minHeight: '2.5em' }}> {/* Adjust '3em' as needed */}
                                 <TypeAnimation
                                     sequence={[
                                         'Jonathan',
@@ -43,10 +43,12 @@ const HeroSection = () => {
                         </p>
                         <div>
                             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 bg-white hover:bg-slate-200 text-white">
-                                Hire Me
+                                LinkedIn
                             </button>
                             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
-                                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
+                                <a href="https://drive.google.com/file/d/1g-O53DpruIwCd3XREKr-bIRxiIEYkMHU/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                                    Download Resume/CV
+                                </a>
                             </button>
                         </div>
                     </div>
