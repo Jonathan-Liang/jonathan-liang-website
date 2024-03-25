276919f7c1d41b4a33f5ce94322a44b0a5f7d234
Your Name <jonathanliang88@gmail.com>
2024-03-25 03:02:25 -0400

    adjusted padding

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -27,13 +27,13 @@ const achievementsList = [
 
 const AchievementsSection = () => {
   return (
-    <div className="py-2 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="sm:border-[#33353F] sm:border rounded-md py-2 px-16 flex flex-col sm:flex-row items-center justify-between">
+    <div className="py-1 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="sm:border-[#33353F] sm:border rounded-md py-1 px-16 flex flex-col sm:flex-row items-center justify-between">
         {achievementsList.map((achievement, index) => {
           return (
             <div
               key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+              className="flex flex-col items-center justify-center mx-4 my-1 sm:my-0"
             >
               <h2 className="text-white text-4xl font-bold flex flex-row">
                 {achievement.prefix}
