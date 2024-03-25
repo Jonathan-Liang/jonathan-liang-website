6889dadbf28b1869ef80448fbcd23f2479999dfd
Your Name <jonathanliang88@gmail.com>
2024-03-24 22:09:25 -0400

    made header more readable, fixed top navigation bar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
                     className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
                 >
                     <div className="col-span-7 place-self-center text-center sm:text-left">
-                        <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
+                        <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
                             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I'm {" "}</span>
                             <br></br>
                             <TypeAnimation
