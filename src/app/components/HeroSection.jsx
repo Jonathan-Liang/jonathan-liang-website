eb5b0f1845214eacb4dff30631fbc218771f02d2
Jonathan Liang <jonathanliang88@gmail.com>
2024-06-29 13:40:09 -0400

    updated profile picture

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -62,8 +62,8 @@ const HeroSection = () => {
                     <div className="col-span-5 place-self-center mt-4 lg:mt-0">
                         <div className="rounded-full bg-[#181818] w-[250px] h-[250px]  lg:w-[250px] lg:h-[250px] relative">
                             <Image
-                                src="/images/hero-image.png"
-                                alt="hero image"
+                                src="/images/personal-pic.png"
+                                alt="picture of me :)"
                                 className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                                 width={300}
                                 height={300}
